1d1bb4fb6a4d6a6de7d1a37f99cd1cdd4101aabb
NixtonVallejos <ossielmiranda33@gmail.com>
2021-11-25 10:19:13 -0600

    add paciente

---
+++ app/Http/Controllers/PacienteController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paciente;
+use Illuminate\Http\Request;
+
+class PacienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('Paciente.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $paciente = new Paciente();
+        $paciente->name = $request->get('name');
+        $paciente->cedula = $request->get('cedula');
+        $paciente->birth = $request->get('birth');
+        $paciente->municipio_id = $request->get('municipio_id');
+        $paciente->save();
+
+        return redirect('/Paciente');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $paciente = Paciente::find($id);
+        return view('Paciente.edit')->with('Paciente',$paciente);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $paciente = Paciente::find($id);
+        $paciente->name = $request->get('name');
+        $paciente->cedula = $request->get('cedula');
+        $paciente->birth = $request->get('birth');
+        $paciente->municipio_id = $request->get('municipio_id');
+        $paciente->save();
+
+        return redirect('/Paciente');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
